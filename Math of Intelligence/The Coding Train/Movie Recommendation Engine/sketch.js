568a4f153d9ba26adfaa07c109f4dc504beef6be
shaunak1105 <shaunak1105@gmail.com>
2018-09-10 02:27:14 +0530

    Interface for similarity score generation

--- Math of Intelligence/The Coding Train/Movie Recommendation Engine/sketch.js
+++ Math of Intelligence/The Coding Train/Movie Recommendation Engine/sketch.js
@@ -6,5 +6,33 @@ function preload(){
 
 function setup(){
     noCanvas();
-    console.log(data)
+    //console.log(data);
+
+    var users = data.users;
+
+    // using p5.js to create dropdowns
+
+    var dropdown1 = createSelect('');
+    var dropdown2 = createSelect('');
+
+    for (var i = 0; i < users.length; ++i){
+
+        // populate the dropdowns with names
+        dropdown1.option(users[i].name);
+        dropdown2.option(users[i].name);
+    }
+
+    // create a button
+
+    var button = createButton('Submit');
+
+    button.mousePressed(euclideanSimilarity);
+
+
+    function euclideanSimilarity(){
+        var person1 = dropdown1.value();
+        var person2 = dropdown2.value()
+
+        console.log(person1, person2);
+    }
 }
