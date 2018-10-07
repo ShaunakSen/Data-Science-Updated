bfd1696cad13e85e8c2ec39c2ecb95125ec98c30
shaunak1105 <shaunak1105@gmail.com>
2018-10-08 00:02:29 +0100

    upgraded to es6

--- Math of Intelligence/The Coding Train/Neural Network JS/matrix.js
+++ Math of Intelligence/The Coding Train/Neural Network JS/matrix.js
@@ -1,71 +1,79 @@
-function Matrix(rows, cols){
-    this.rows = rows;
-    this.cols = cols;
-    this.matrix = [];
+class Matrix {
 
-    // loop through each row
-    for (var i = 0; i < this.rows; ++ i){
-        // every single row is also an array
-        this.matrix[i] = [];
-        for (var j = 0; j < this.cols; ++j){
-            // initialize each value to 0
-            this.matrix[i][j] = 0;
-        }
-    }
-}
-
-// randomly populate values of the matrix
-
-Matrix.prototype.randomize = function(){
-    for(var i = 0; i < this.rows; ++i){
-        for(var j = 0; j < this.cols; ++j){
-            this.matrix[i][j] = Math.floor(Math.random() * 10);
-        }
-    }
-}
-
-
-// addition function
-
-Matrix.prototype.add = function(n){
-
-    // check if n is a matrix
-
-    if (n instanceof Matrix){
-        for(var i = 0; i < this.rows; ++i){
-            for(var j = 0; j < this.cols; ++j){
-                this.matrix[i][j] += n.matrix[i][j];
-            }
-        }
-    } else 
-    // n is just a scalar number 
-    {
-        for(var i = 0; i < this.rows; ++i){
-            for(var j = 0; j < this.cols; ++j){
-                this.matrix[i][j] += n;
-            }
-        }
-    }
-
-}
-
-// scalar multiplication function
-
-Matrix.prototype.multiply = function(n){
+    constructor(rows, cols){
+        this.rows = rows;
+        this.cols = cols;
+        this.matrix = [];
     
-    if (n instanceof Matrix){
-        for(var i = 0; i < this.rows; ++i){
-            for(var j = 0; j < this.cols; ++j){
-                this.matrix[i][j] *= n.matrix[i][j];
-            }
-        }
-    } else {
-        for(var i = 0; i < this.rows; ++i){
-            for(var j = 0; j < this.cols; ++j){
-                this.matrix[i][j] *= n;
+        // loop through each row
+        for (let i = 0; i < this.rows; ++ i){
+            // every single row is also an array
+            this.matrix[i] = [];
+            for (let j = 0; j < this.cols; ++j){
+                // initialize each value to 0
+                this.matrix[i][j] = 0;
             }
         }
     }
     
-}
+    // randomly populate values of the matrix
+    
+    randomize(){
+        for(let i = 0; i < this.rows; ++i){
+            for(let j = 0; j < this.cols; ++j){
+                this.matrix[i][j] = Math.floor(Math.random() * 10);
+            }
+        }
+    }
+    
+    
+    // addition function
+    
+    add(n){
+    
+        // check if n is a matrix
+    
+        if (n instanceof Matrix){
+            for(let i = 0; i < this.rows; ++i){
+                for(let j = 0; j < this.cols; ++j){
+                    this.matrix[i][j] += n.matrix[i][j];
+                }
+            }
+        } else 
+        // n is just a scalar number 
+        {
+            for(let i = 0; i < this.rows; ++i){
+                for(let j = 0; j < this.cols; ++j){
+                    this.matrix[i][j] += n;
+                }
+            }
+        }
+    
+    }
+    
+    // scalar multiplication function
+    
+    multiply(n){
+    
+        if (n instanceof Matrix){
+            for(let i = 0; i < this.rows; ++i){
+                for(let j = 0; j < this.cols; ++j){
+                    this.matrix[i][j] *= n.matrix[i][j];
+                }
+            }
+        } else {
+            for(let i = 0; i < this.rows; ++i){
+                for(let j = 0; j < this.cols; ++j){
+                    this.matrix[i][j] *= n;
+                }
+            }
+        }
+        
+    }
+
+
+} //--------------------- end of class 
+
+
+
 
--- Math of Intelligence/The Coding Train/Neural Network JS/nn.js
+++ Math of Intelligence/The Coding Train/Neural Network JS/nn.js
@@ -1,8 +1,11 @@
-// constructor function
+class NeuralNetwork{
 
-function NeuralNetwork(numI, numH, numO){
-    this.input_nodes = numI;
-    this.hidden_nodes = numH;
-    this.output_nodes = numO;
+    // constructor function
+
+    constructor(input_nodes, hidden_nodes, output_nodes){
+        this.input_nodes = input_nodes;
+        this.hidden_nodes = hidden_nodes;
+        this.output_nodes = output_nodes;
+    }
 
 }
